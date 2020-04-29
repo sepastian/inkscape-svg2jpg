bf256abeb4fe14cd316a12d73598e8662ed2ec45
madaky <17172989+madaky@users.noreply.github.com>
2020-04-30 00:00:39 +0530

    This was causing error in Ubuntu 20.04 Focal

--- svg2jpg.sh
+++ svg2jpg.sh
@@ -12,4 +12,4 @@ PNG_TMP_FILE="/tmp/$(mktemp 'svg2jpg.png.XXXXX')"
 inkscape --export-png="${PNG_TMP_FILE}" --export-area-page "${SVG_TMP_FILE}" 2&>1 /dev/null
 
 # Convert temporary PNG file to JPG on STDOUT, delete temporary PNG file.
-convert ephemeral:"${PNG_TMP_FILE}" jpeg:-
+convert "${PNG_TMP_FILE}" jpeg:-
